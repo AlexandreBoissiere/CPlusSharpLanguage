a57abce4176dc56dd2de32210963b74d4be32441
AlexandreBoissiere <alexgf27550@gmail.com>
2019-10-22 20:01:57 +0200

    Mise à jour du fichier de code du gestionnaire de mémoire.

--- Resources/MemoryManager.cs
+++ Resources/MemoryManager.cs
@@ -14,32 +14,65 @@ namespace MemoryManager
     {
         static void Main(string[] args)
         {
+            Process curr = Process.GetCurrentProcess();
+
+            Console.WriteLine("Name of the process to track (Type 'self' to track this process) : ");
+            string process = Console.ReadLine();
+
+
             while (true)
             {
-                Process curr = Process.GetCurrentProcess();
+                if (process == "self")
+                    curr = Process.GetCurrentProcess();
+                else
+                {
+                    try
+                    {
+                        Process[] procs;
+                        procs = Process.GetProcessesByName(process);
+                        curr = procs[0];
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.WriteLine("Press any key to quit");
+                        Console.ReadKey(true);
+                        Environment.Exit(1);
+                    }
+                }
+
                 ComputerInfo CI = new ComputerInfo();
 
-                object[] categories = new object[5] { "Process name", "Id", "Private memory", "WorkingSet", "Virtual memory" };
-                object[] separators = new object[5] { "=============", "=====", "==============", "=============", "===============" };
-                object[] parametersCurrProc = new object[5] { curr.ProcessName, curr.Id, curr.PrivateMemorySize64, curr.WorkingSet64, curr.VirtualMemorySize64 };
+                float physMemUsageProto = ((float)curr.WorkingSet64 / (float)CI.TotalPhysicalMemory) * 100;
+                float physMemUsage = (float)Math.Round(physMemUsageProto, 2);
 
-                object[] categoriesM = new object[6] { "Machine name", "Address", "Av. physical memory", "Tot. physical memory", "Av. virtual memory", "Tot. virtual memory"};
-                object[] separatorsM = new object[6] { "===============", "=======", "===================", "====================", "===================", "===================="};
+                float virtMemUsageProto = ((float)curr.VirtualMemorySize64 / (float)CI.TotalVirtualMemory) * 100;
+                float virtMemUsage = (float)Math.Round(virtMemUsageProto, 2);
+
+                object[] categories = new object[7] { "Process name", "Id", "Private memory", "WorkingSet", "Virtual memory", "Phys mem usage", "Virt mem usage" };
+                object[] parametersCurrProc = new object[7] { curr.ProcessName, curr.Id, curr.PrivateMemorySize64, curr.WorkingSet64, curr.VirtualMemorySize64, physMemUsage + " %", virtMemUsage
+                + " %" };
+
+                object[] categoriesM = new object[6] { "Machine name", "Address", "Av. physical memory", "Tot. physical memory", "Av. virtual memory", "Tot. virtual memory" };
                 object[] parametersComputer = new object[6] { Environment.MachineName, "::1", CI.AvailablePhysicalMemory, CI.TotalPhysicalMemory, CI.AvailableVirtualMemory, CI.TotalVirtualMemory };
 
                 Console.Clear();
 
-                Console.WriteLine(string.Format(CultureInfo.CurrentCulture, "{0,-10}    {1,-10}{2,-10}   {3,-10}\t   {4,-10}", categories));
-                Console.WriteLine(string.Format(CultureInfo.CurrentCulture, "{0,-10}   {1,-10}{2,-10}   {3,-10}   {4,-10}", separators));
-                Console.WriteLine(string.Format(CultureInfo.CurrentCulture,"{0,-10}   {1,-10}{2,-10}       {3,-10}\t   {4,-10}", parametersCurrProc));
+                Console.WriteLine("=========Process=========");
+                for (int i = 0; i < categories.Length; i++)
+                {
+                    Console.WriteLine("{0} : {1}", categories[i], parametersCurrProc[i]);
+                }
 
                 Console.WriteLine("\n\n");
 
-                Console.WriteLine(string.Format(CultureInfo.CurrentCulture, "{0,-10}      {1,-10}{2,-10}   {3,-10}\t {4,-10}   {5,-10}", categoriesM));
-                Console.WriteLine(string.Format(CultureInfo.CurrentCulture, "{0,-10}   {1,-10}{2,-10}   {3,-10}   {4,-10}  {5,-10}", separatorsM));
-                Console.WriteLine(string.Format(CultureInfo.CurrentCulture, "{0,-10}   {1,-10}{2,-10}       \t  {3,-10}\t         {4,-10}\t      {5,-10}", parametersComputer));
+                Console.WriteLine("=========Computer=========");
+                for (int i = 0; i < categoriesM.Length; i++)
+                {
+                    Console.WriteLine("{0} : {1}", categoriesM[i], parametersComputer[i]);
+                }
 
-                Console.WriteLine("\n\n\n\n\n'Av.' = Available ; 'Tot.' = Total");
+                Console.WriteLine("\n\n\n\n\n'Av.' = Available ; 'Tot.' = Total \n'Phys' = Physical ; 'Virt' = Virtual");
                 Thread.Sleep(1000);
             }
         }
