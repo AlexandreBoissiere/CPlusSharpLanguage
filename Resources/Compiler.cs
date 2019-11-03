0e9ff21529a125d87fda395f62a421c821154e78
AlexandreBoissiere <alexgf27550@gmail.com>
2019-11-03 12:34:06 +0100

    Ajout du compileur indépendant pour compiler un fichier de codeCPlusSharp '.cps'.

---
+++ Resources/Compiler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+
+namespace CS_Compiler
+{
+    class Compiler
+    {
+        static void Main(string[] args)
+        {
+            CodeDomProvider provider = CSharpCodeProvider.CreateProvider("CSharp");
+
+            string InputFileName = @"";
+            Console.WriteLine("Path of the file to compile : ");
+            InputFileName = Console.ReadLine();
+
+            string input = System.IO.File.ReadAllText(InputFileName);
+
+            string Output = "";
+            Console.WriteLine("Output file name : ");
+            Output = Console.ReadLine();
+
+            CompilerParameters parameters = new CompilerParameters();
+
+            parameters.GenerateExecutable = true;
+
+            parameters.OutputAssembly = Output;
+
+            CompilerResults results = provider.CompileAssemblyFromSource(parameters, input);
+
+            if (results.Errors.Count > 0)
+            {
+                foreach (CompilerError CompErr in results.Errors)
+                {
+                    Console.WriteLine("Error on line "+CompErr.Line + "; Error number : "+CompErr.ErrorNumber + "Context : "+CompErr.ErrorText);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Succeful compilation ! ");
+                Console.WriteLine("Type something to exit.");
+                Console.ReadKey(false);
+            }
+        }
+    }
+}
